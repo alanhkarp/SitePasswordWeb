fb55596f06a546e196151cb779ecfbcdd39bc475
alanhkarp <alanhkarp@gmail.com>
2023-09-12 15:48:34 -0700

    Reset settings to default after test
    It looked like the saved settings had become the new default because they showed up after running the save new defaults test.  That was because I forgot to rest them when the test was over.

--- src/test.js
+++ src/test.js
@@ -4,8 +4,8 @@ alert("Test code loaded");
 function testCalculation() {
     const password = "qwerty";
     const expected = "pvQhUS2xVu0I";
-    const $superpw = document.getElementById("superpw");
-    const $sitepw = document.getElementById("sitepw");
+    const $superpw = get("superpw");
+    const $sitepw = get("sitepw");
     $superpw.value = password;
     $superpw.onkeyup();
     actual = $sitepw.value;
@@ -18,21 +18,58 @@ function testCalculation() {
 // Test save as default
 function testSaveAsDefault() {
     localStorage.restart = "testSaveAsDefault";
-    console.log("database", SitePassword.database);
+    get("pwlength").value = 15;
+    get("allowspecialcheckbox").click();
+    get("specials").value = "%^&";
+    get("makedefaultbutton").click();
 }
 function testSaveAsDefault2() {
-    console.log("testSaveAsDefault restarted", SitePassword.database);
+    let tests = get("pwlength").value === "15";
+    tests = tests && get("allowspecialcheckbox").checked;
+    tests = tests && get("specials").value === "%^&";
+    if (tests) {
+        console.log("Passed: Test save as default");
+    } else {
+        console.error("Failed: Test save as default", "15", "|" + get("pwlength").value + "|");
+    }
+    // Put things back
+    get("pwlength").value = 12;
+    get("allowspecialcheckbox").click();
+    get("specials").value = "/!=@?._-";
+}
+function testRememberSettings() {
+    localStorage.restart = "testRememberSettings";
+    get("sitename").value = "site";
+    get("username").value = "user";
+    get("remember").click();
+}
+function testRememberSettings2() {
+    let tests = get("sitename").value === "site";
+    tests = tests && get("username").value === "user";
+    if (tests) {
+        console.log("Passed: Test remember settings");
+    } else {
+        console.error("Failed: Test remember settings", "site", "|" + get("sitename").value + "|");
+    }
 }
 // Run all tests
 function runTests() {
     let restart = localStorage.restart;
     if (!restart) {
         testCalculation();
+        testRememberSettings();
         testSaveAsDefault();
     } else {
         if (restart === "testSaveAsDefault") {
             testSaveAsDefault2();
             localStorage.removeItem("restart");
+        } else if (restart === "testRememberSettings") {
+            testRememberSettings2();
+            localStorage.removeItem("restart");
         }
     }
+}
+// Utility functions
+function get(id) {
+    return document.getElementById(id);
 }
