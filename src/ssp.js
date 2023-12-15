a31aefc10c60e299d6971e7c9575593168b22bc4
alanhkarp <alanhkarp@gmail.com>
2023-12-15 14:49:35 -0800

    Get rid of entropy estimate
    It's not accurate enough to give much more information than just the 5 possible scores.

--- src/ssp.js
+++ src/ssp.js
@@ -269,9 +269,7 @@ let SitePasswordWeb = ((function (self) {
             const $input = get(which);
             const report = zxcvbn($input.value);
             $meter.value = report.score;
-            // The value 1.8 comes from an estimate of how much zxcvbn multiplies the number of
-            // guesses for each additional character in the password vs. using log2(alphabet size)
-            $meter.title = strengthText[report.score] + "(" + Math.round(1.8*Math.log2(report.guesses)) + ")";
+            $meter.title = strengthText[report.score];
             $input.style.color = strengthColor[report.score];
         }
         $superpw.onkeyup = function () {
