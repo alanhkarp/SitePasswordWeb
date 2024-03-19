cd37408be321fa44e7957e29727c807e0368dbf4
alanhkarp <alanhkarp@gmail.com>
2024-03-19 13:41:43 -0700

    Change handling of conditional await
    In my other code, I put await Promise.resolve() at the start of any function that doesn't await on all branches.  I changed this function to conform to that style.

--- src/bg.js
+++ src/bg.js
@@ -137,8 +137,9 @@ let SitePassword = ((function (self) {
         return valOK;
     }
     async function computePassword(superpw, salt, settings) {
+        await Promise.resolve(); // Because some branches have await and others don't
         if (!(settings.allowupper || settings.allowlower || settings.allownumber || settings.allowspecial)) {
-            return await Promise.resolve("");
+            return "";
         }
         let args = {"pw": superpw, "salt": salt, "settings": settings, "iters": 200_000, "keysize": settings.pwlength * 8};
         let pw = await candidatePassword(args);
